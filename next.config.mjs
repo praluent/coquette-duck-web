8a63194f7d827b97a6ff12b8385c2d971d808998
Ali Syed <148507779+praluent@users.noreply.github.com>
2024-03-10 22:33:50 +0000

    disable eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = {
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  }
+const nextConfig = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
+
+};
+
 export default nextConfig;
